014bf71816073383755893f9d694b90cd186d721
bisthebis <bism25@hotmail.com>
2017-03-03 21:44:28 +0100

    main now takes a file as argument and scan it

--- scanner/main.cpp
+++ scanner/main.cpp
@@ -1,9 +1,30 @@
 #include <QtCore>
+#include "token.h"
+#include "scanner.h"
+#include "myexception.h"
 
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
-    QTimer::singleShot(1000, [&](){qDebug() << "Leaving..."; a.exit();});
+    try {
+        QString targetFile;
+        if (argc == 2)
+            targetFile = a.arguments().at(1);
+        else
+            throw MyException("Wrong argument count");
+        Scanner scan(targetFile);
 
-    return a.exec();
+        while (true) {
+            auto t = scan.nextToken();
+            if (t.type == Token::END_OF_FILE)
+                break;
+            qDebug() << t.toString();
+        }
+    }
+    catch (MyException& e) {
+        qDebug() << "Exception occured : " << e.msg;
+    }
+
+
+    return 0;
 }

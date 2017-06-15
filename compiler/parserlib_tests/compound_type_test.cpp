9ee9db2ab824be2fe34a6861e95233d045b24099
bisthebis <bism25@hotmail.com>
2017-06-15 16:13:26 +0200

    Renamed to include primitive type

--- compiler/parserlib_tests/compound_type_test.cpp
+++ compiler/parserlib_tests/compound_type_test.cpp
@@ -1,5 +1,5 @@
 #include "compound_type_test.h"
-#include "compound_type.h"
+#include "type.h"
 #include <QMap>
 #include <QString>
 #include <QtTest/QtTest>
@@ -25,22 +25,3 @@ void CompoundTypeTest::enumConversion() {
     }
 }
 
-
-void CompoundTypeTest::structConversion() {
-    /* Sample to pseudo-check :
-     * struct Foo {
-     *      Int x;
-     *      Float y;
-     * */
-    QMap<QString, int> values;
-    values["RED"] = 1;
-    values["BLUE"] = 5;
-    values["GREEN"] = 3;
-
-    CompoundType type = CompoundType::Enum("Color", values);
-    QVERIFY(type.name() == "Color");
-    const auto valuesEnd = values.cend();
-    for (auto it = values.cbegin(); it != valuesEnd; ++it) {
-        QVERIFY(*it == type.enumValues()[it.key()]);
-    }
-}
